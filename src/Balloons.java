8752306fa7137c4556492c110d65cfb17650b4c5
ghiorsiandrea <andreaghiorsims@gmail.com>
2022-10-23 22:01:02 -0300

    Ex. 1 Balloons - solved

--- src/Balloons.java
+++ src/Balloons.java
@@ -9,14 +9,14 @@ public class Balloons {
     public static void main(String[] args) throws IOException {
 
         // lee datos del archivo "./resources/balloons.txt"
-        Scanner sc = new Scanner(new File("./resources/balloons.txt"));
+        Scanner sc = new Scanner(new File("./resources/balloons/balloons.txt"));
 
         // abre el archivo para grabar en "./resources/balloons-result.txt"
-        PrintWriter pw = new PrintWriter(new FileWriter("./resources/balloons-result.txt"));
+        PrintWriter pw = new PrintWriter(new FileWriter("./resources/balloons/balloons-result.txt"));
 
         int cases = sc.nextInt();
         sc.nextLine();
-        for (int i = 0; i < cases; i++) {
+        for (int i = 1; i < cases + 1; i++) {
             processCase(sc, pw, i);
         }
 
@@ -26,12 +26,13 @@ public class Balloons {
     public static void processCase(Scanner sc, PrintWriter pw, int nCase) throws IOException {
 
         // lee un cases entero del archivo
-        String cost = sc.nextLine();
-        int greenCost = cost.charAt(0);
-        int purpleCost = cost.charAt(2);
+        String[] costs = sc.nextLine().split(" ");
+        int greenCost = Integer.parseInt(costs[0]);
+        int purpleCost = Integer.parseInt(costs[1]);
         int biggerCost = Math.max(greenCost, purpleCost);
         int smallestCost = Math.min(greenCost, purpleCost);
         int participants = sc.nextInt();
+        sc.nextLine();
         int total1Left = 0;
         int total1Rigth = 0;
         int iLeft;
@@ -39,10 +40,9 @@ public class Balloons {
 
 
         for (int i = 1; i < participants + 1; i++) {
-            sc.nextLine();
-            String iresults = sc.nextLine();
-            iLeft = iresults.charAt(0);
-            iRigth = iresults.charAt(2);
+            String[] iresults = sc.nextLine().split(" ");
+            iLeft = Integer.parseInt(iresults[0]);
+            iRigth = Integer.parseInt(iresults[1]);
             if (iLeft == 1) {
                 total1Left++;
             }
@@ -51,10 +51,10 @@ public class Balloons {
             }
         }
         int max1 = Math.max(total1Left, total1Rigth);
-        int min1 = Math.min(total1Rigth, total1Rigth);
+        int min1 = Math.min(total1Left, total1Rigth);
         int minimumCost = max1 * smallestCost + min1 * biggerCost;
 
         // ejemplo de grabar ese cases en el archivo de salida:
-        pw.println("Case #1: " + minimumCost);
+        pw.println("Case #" + nCase + ": " + minimumCost);
     }
 }
